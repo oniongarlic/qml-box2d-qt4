fa043ede7de03d640d37d28046a7bd256ca0fe88
Kaj-Michael Lang <milang@tal.org>
2012-10-24 16:20:48 +0300

    RevoluteJoint: Remove release/grab code as base joint handles it now

--- box2drevolutejoint.cpp
+++ box2drevolutejoint.cpp
@@ -177,20 +177,6 @@ void Box2DRevoluteJoint::createJoint()
     mInitializePending = false;
 }
 
-void Box2DRevoluteJoint::release()
-{
-    if (!mReleased)
-        mReleased = true;
-    cleanup(world());
-}
-
-void Box2DRevoluteJoint::grab()
-{
-    if (mReleased)
-        mReleased = false;
-    createJoint();
-}
-
 void Box2DRevoluteJoint::cleanup(b2World *world)
 {
     if (mRevoluteJoint && bodyA() && bodyB()) {
